48103f22fa84a0d21d34b6cd1e593bfc463e4fa6
elton <elton.nicolau@innovareti.com.br>
2024-10-13 12:04:25 -0300

    ajusets

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,10 @@ module.exports = {
   },
   extends: ["plugin:vue/essential", "@vue/prettier"],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "vue/multi-word-component-names": "off",
+      //"no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    //"no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+         "no-console":"off",
+          "no-debugger": "off",
+	  "vue/multi-word-component-names": "off",
   },
 };
